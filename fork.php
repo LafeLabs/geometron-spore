850f1bcdb9d83d7d2c2efd501837974d9c2b2d66
Trash Robot <lafelabs@gmail.com>
2026-07-11 11:56:07 -0600

    fork updated

--- fork.php
+++ fork.php
@@ -1,41 +1,26 @@
 <?php
 
-if(isset($_GET["fork"])){
-    $fork = $_GET["fork"];
+$fork = isset($_GET["fork"]) ? $_GET["fork"] : "fork";
+
+if (!is_dir($fork)) {
+    mkdir($fork);
 }
-else{
-    $fork = "cold-switch-network";
+
+$files = json_decode(file_get_contents('spore.json'), true);
+
+foreach ($files as $file) {
+    copy($file, $fork . "/" . $file);
 }
-
-mkdir($fork);
-
-@copy("geometron.html",$fork."/geometron.html");
-@copy("geometron.css",$fork."/geometron.css");
-@copy("geometron.js",$fork."/geometron.js");
-@copy("hypercube.json",$fork."/hypercube.json");
-@copy("sketch.js",$fork."/sketch.js");
-@copy("geometron-glyph-feed.html",$fork."/geometron-glyph-feed.html");
-
-@copy("index.html",$fork."/index.html");
-@copy("editor.html",$fork."/editor.html");
-@copy("save-file.php",$fork."/save-file.php");
-@copy("save-file-get.php",$fork."/save-file-get.php");
-@copy("load-file.php",$fork."/load-file.php");
-@copy("list-files.php",$fork."/list-files.php");
-@copy("list-directories.php",$fork."/list-directories.php");
-@copy("README.md",$fork."/README.md");
-@copy("fork.php",$fork."/fork.php");
-@copy("wall.txt",$fork."/wall.txt");
 
 ?>
-<a href = "<?php echo $fork?>/index.html"><?php echo $fork?>/index.html</a>
+<a href="<?php echo $fork?>/index.html"><?php echo $fork?>/index.html</a>
 <style>
-body{
-    font-size:3em;
-    font-family:arial;
+body {
+    font-size: 3em;
+    font-family: arial;
 }
-a{
-    font-size:3em;
-    color:blue;
+a {
+    font-size: 3em;
+    color: blue;
 }
 </style>
